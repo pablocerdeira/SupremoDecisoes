3ab9701a7e4de324851a0cc406c8d58ff99d2bdf
Pablo Cerdeira <pablo.cerdeira@gmail.com>
2013-04-19 18:10:01 -0300

    Setting global variables

--- main.py
+++ main.py
@@ -34,16 +34,16 @@ from MySQLtools import *
 #  - Debug = 0: no debug
 #  - Debug = 1: return basic information
 #  - Debug = 2: return detailed information
-debug = 2
+global debug = 2
 
 # MySQL settings
-MySQLhost =     '172.16.4.51'
-MySQLusername = 'pablocerdeira'
-MySQLpasswd =   'pablo123'
-MySQLdb =       'fredericodba'
+global MySQLhost =     '172.16.4.51'
+global MySQLusername = 'pablocerdeira'
+global MySQLpasswd =   'pablo123'
+global MySQLdb =       'fredericodba'
 
 # Rows limit when reading/writing from/to MySQL
-MySQLLimit =    'limit 0,1000'
+global MySQLLimit =    'limit 0,1000'
 
 # Analysis tables
 # We are going to adopt some table prefixies
@@ -54,24 +54,24 @@ MySQLLimit =    'limit 0,1000'
 
 # Main table
 # Define table name, source table and table indexes (idx)
-ta_main =               "ta_main"
-ta_main_source =        'monocraticas'
-ta_main_idx =           ['num_processo','ministro']
+global ta_main =               "ta_main"
+global ta_main_source =        'monocraticas'
+global ta_main_idx =           ['num_processo','ministro']
 
 
 # Table: Analysis Words All
 # Define table name, table columns and table indexes (idx)
-ta_words_all =          "ta_words_all"           
-ta_word_all_columns =   ['id int not null auto_increment',
-                        'id_monocratica int null',
-                        'word varchar(255) null',
-                        'word_count int null'
-                        ]
-ta_words_all_idx =      ['id_monocratica','word','word_count']
+global ta_words_all =          "ta_words_all"           
+global ta_word_all_columns =   ['id int not null auto_increment',
+                                'id_monocratica int null',
+                                'word varchar(255) null',
+                                'word_count int null'
+                                ]
+global ta_words_all_idx =       ['id_monocratica','word','word_count']
 
 # Export plain texts decisions saving it to disk
-exporToDisk = False
-exportFolder = '/home/pablocerdeira/corpora/decisoes_supremo/'
+global exporToDisk = False
+global exportFolder = '/home/pablocerdeira/corpora/decisoes_supremo/'
 
 # Steps to run or ignore
 connectDB =         True
