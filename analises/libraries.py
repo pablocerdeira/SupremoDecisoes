53f03cee1b5216ed5f91352952b9f63b39cac4f7
Pablo Cerdeira <pablocerdeira@debian.cloud.fgv.br>
2013-04-23 13:36:45 -0300

    RM analises

--- analises/libraries.py
+++
@@ -1,9 +0,0 @@
-import MySQLdb as db
-from MySQLdb.cursors import CursorUseResultMixIn,  DictCursor, SSDictCursor, SSCursor
-import pandas
-import pandas.io.sql as psql
-import numpy as np
-import cPickle as pickle
-import gzip
-from multiprocessing import Pool
-import codecs
--- analises/settings.py
+++
@@ -1,8 +0,0 @@
-MySQLhost =     '172.16.4.51'
-MySQLusername = 'pablocerdeira'
-MySQLpasswd =   'pablo123'
-MySQLdb =       'fredericodba'
-
-debug = 1
-
-conn, cur = 0
