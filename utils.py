95d63a1a9faa366cd6af5ef29ee4c23d206c5253
Pablo Cerdeira <pablo.cerdeira@gmail.com>
2013-04-23 11:51:19 -0300

    Small bugs

--- utils.py
+++ utils.py
@@ -41,8 +41,8 @@ def wordFrequence(text):
 def exportTable(filename,filetype='csv'):
     
     csv_writer = libs.csv.writer(open('./exports/'+filename+'.csv', "wt"))
-    csv_writer.writerow([i[0] for i in st.cur.description])
-    csv_writer.writerows(st.rows)
+    writer.writerow(list(st.rows.viewkeys()))
+    writer.writerow(list(st.rows.viewvalues()))
     del csv_writer
 
 
