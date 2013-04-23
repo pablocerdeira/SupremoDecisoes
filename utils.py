30ad14ff2a79637e94388a346c3a0fd03cbbf246
Pablo Cerdeira <pablo.cerdeira@gmail.com>
2013-04-23 11:48:52 -0300

    Texting exports

--- utils.py
+++ utils.py
@@ -42,7 +42,7 @@ def exportTable(filename,filetype='csv'):
     
     csv_writer = libs.csv.writer(open('./exports/'+filename+'.csv', "wt"))
     csv_writer.writerow([i[0] for i in st.cur.description])
-    csv_writer.writerows(st.cur)
+    csv_writer.writerows(st.rows)
     del csv_writer
 
 
